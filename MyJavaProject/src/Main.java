3a9dbe4119342b0a27a2077e037a2beae993be1e
Vipin Gurnani <vipingurnani1@gmail.com>
2025-01-30 15:25:40 -0500

    Made a minor change to trigger GitHub Actions workflow

--- MyJavaProject/src/Main.java
+++ MyJavaProject/src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome to github actions!");
+        System.out.printf("Hello and welcome to11 github actions!");
 
     }
 }
